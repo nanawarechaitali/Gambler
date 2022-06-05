2522f4cb790ca5e02def346cb0022396ac7b581a
nanawarechaitali <nanawarechaitali4196@gamil.com>
2022-06-06 02:56:40 +0530

    Added

---
+++ src/com/bridgelabz/Gambler.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Gambler {
+    static int amount=100;
+    final static int BET = 1;
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the master of Gambler");
+        System.out.println("total amount "+amount);
+        System.out.println("betting amount "+BET);
+
+    }
+}
