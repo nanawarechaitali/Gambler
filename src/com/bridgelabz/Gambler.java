1e4053a1f1d2530165eb467306e771887b7a301b
nanawarechaitali <nanawarechaitali4196@gamil.com>
2022-06-06 03:05:33 +0530

    ADDED

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -1,4 +1,5 @@
 package com.bridgelabz;
+import java.util.Random;
 
 public class Gambler {
     static int amount=100;
@@ -8,6 +9,17 @@ public class Gambler {
         System.out.println("Welcome to the master of Gambler");
         System.out.println("total amount "+amount);
         System.out.println("betting amount "+BET);
+        Random random = new Random();
+        int betting = random.nextInt(2);
+        if(betting == 0){
+            System.out.println("You loose the bet");
+            amount--;
+            System.out.println("amount remaining "+amount);
+        }else{
+            System.out.println("You won the bet ");
+            amount++;
+            System.out.println("You new amount is "+amount);
+        }
 
     }
 }
